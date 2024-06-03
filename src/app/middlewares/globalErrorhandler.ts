8a5aa3da5668f1178215a1e7e2f5f6dbdccdf41a
mehedi37m <mz.mehedi37@gmail.com>
2024-06-04 01:16:31 +0600

    error hanlding

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -1,9 +1,30 @@
-import  { NextFunction, Request, Response } from 'express';
+import  { ErrorRequestHandler} from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (err:any, req: Request, res: Response, next:NextFunction) => {
+let globalErrorHandler : ErrorRequestHandler = (err, req , res , next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message ||'something went wrong';
      
+  type TErrorSource = {
+ 
+    path:string | number;
+    message:string ;
+  }[];
+
+ let errorSource : TErrorSource = [{
+    path:'',
+    message: message
+  }];
+
+  const handleZodError = (err: ZodError) => {
+      const statusCode = 400;
+  }
+
+      if(err instanceof ZodError){
+        
+        message = "Zod Error now"
+      }
+
     return res.status(statusCode).json({
       success:false,
       message:message,
@@ -12,3 +33,13 @@ const globalErrorHandler = (err:any, req: Request, res: Response, next:NextFunct
   }
 
   export default globalErrorHandler
+
+  /**
+   * success 
+   * message
+   * errorSource:[
+   * path:''
+   * message:""
+   * ]
+   * stack
+   */
